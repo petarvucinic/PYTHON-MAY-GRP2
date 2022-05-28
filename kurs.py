9c72851ce504d3dea21b7b5e12b184fccd014135
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-05-28 12:23:43 +0200

    kurs

--- kurs.py
+++ kurs.py
@@ -2,5 +2,49 @@
 
 
 
+# # ovo je varijabla naziv br
+# brojgodina = 17   # ceeo broj int
+
+# moj_broj = 5.45    #float
+# # '' or ""
+
+# # '     "
+# skola = "gimnazija"   #string
+
+# broj = "123456789"    #string
+# broj2 = 123456789     #int
+
+
+# print("vievhbievhiuehviuhvhieurhiurhvbiurhguirhvhruhgiuer")
+
+# print('Ovo je kurs pajtona')
+
+
+# ime = "Petar"       #string
+
+# print(ime)
+# print("skola")
+
+
+# nekiBroj = 17
+
+# neki_broj = 17
+
+# # neki-broj = 17
+
+
+# broj = 17
+# Broj = 17 
+# broJ = 17
+# BROj = 17
+
+
+# vfhb vhdebverbvbefuvhbruhbrev
+
+# print(broj)
+
+
+
+
 
 
