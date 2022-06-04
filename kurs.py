d271ed74e3b22fe0bdcbfb68e9a6e89412f416a5
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-06-04 13:14:35 +0200

    commit

--- kurs.py
+++ kurs.py
@@ -65,3 +65,113 @@
 # print("Povrsina pravougaonika je: ", stranica_a * stranica_b)
 
 
+
+# --------------
+# cas 3 operatori
+
+# pi = 3.14
+# r = int(input("Unesite poluprecnik: "))
+# print("Povrsina kruga je: ", r**2*pi)
+# print("Obim kruga je: ", 2*r*pi)
+
+
+# domaci
+# 
+
+# print(4 == 99)
+# print(10 == 10)
+# print("cat" != "dog")
+# print("mouse" == "mouse")
+# print(0 or 5 < 4)
+# print(True and True)
+# print(False and True)
+# print(3 and 2 > 1)
+# print(False or True)
+# print(False or False)
+# print(not True)
+# print(not False)
+# print(True and (4 == 4))
+
+# print("lion" == "cat" or 99 != 88)
+# print(1 > 1 or 3 < 4 or 4 > 5 and 2 > 1 and 9 > 8 or 7 < 6)
+
+# print(not 2 > 1 and 3 < 4 or 4 > 5 and 2 > 1 and 9 > 8 or 7 < 6)
+#        false  and  true 
+#                false    or   false
+#                           false   and  true
+#                                   false    and true
+#                                            false     or   false
+#                                                    false
+
+# ----------------------------------------------
+
+# print(bool(-82181))
+# print(bool("gvyudyuv"))
+# print(bool(""))
+
+
+# print(5 > 2)
+
+# print("skola" != "mis")
+
+# and 
+# or 
+# not 
+
+
+# True and True => True
+# True and False
+
+# True or True => True 
+# True or Flase => True
+
+# print(not True)
+# print(not False)
+
+# # ---------------------------
+
+
+# x = 5
+
+# # 10    5 + 5
+# x = x + 5
+
+# # 15   10 + 5
+# x += 5
+
+# # x += 5
+
+# print(x)
+
+# x -= 5
+# x *= 5
+# x /= 5
+
+# x %= 5
+# -------------------
+
+
+# x = int(input("Unesite neki broj: "))
+# y = int(input("Unesite neki broj: "))
+
+# a = (x**2 + y**2)**2
+
+# print("Vrednost izraza je: ", a)
+
+
+# ---------------
+
+# galoni = float(input("Unesite galone: "))
+
+# litri = galoni * 4.54
+
+# print("To je ", litri, "litara")
+
+
+# ----------------------------------
+
+
+
+
+
+
