a5a511723053dfcfd9977518eb833409347e8c4a
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-07-17 12:33:33 +0200

    sarina grupa

---
+++ sara.py
@@ -0,0 +1,50 @@
+
+
+
+# # broj = int(input("Unesite neki broj: "))
+
+# # if broj > 0:
+# #     print("Broj je pozitivan")
+# # elif broj < 0:
+# #     print("Broj  je negativan")
+
+
+# # --------------------------------------------------------
+
+
+# # kvadr - a * a
+# # prav - a * b
+
+
+# a = int(input("Unesite duzinu stranice: "))
+# b = int(input("Unesite duzinu stranice: "))
+
+
+# if a == b:
+#     print("Povrsina kvardata je: ", a*b)
+# elif a != b:
+#     print("Povrsina pravougaonika je: ", a*b)
+
+
+
+# rec = "pajtona1234567890"
+# #      0123456
+
+# print(rec[0]) #karakter p
+# print(rec[6]) #karakter 0
+# print(len(rec))
+
+# - zadatak
+
+
+
+
+
+
+
+
+
+
+
+
+
